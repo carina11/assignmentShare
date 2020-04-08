691ed8c21d2101ed58b4fff61c0978b0973844a8
Ryo Inagaki <ryo.inagaki.ryo@gmail.com>
2020-04-09 00:07:59 +0900

    課題の情報が保存できるようになった

--- app/app.py
+++ app/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import subprocess
+import os
+#from datetime import datetime
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("Assignment_List.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    deadline = request.form['deadline']
+    assignmentName = request.form['assignmentName']
+    time = request.form['time']
+
+    print(deadline)
+    print(assignmentName)
+    if(os.path.isdir("assignment") == False):
+        os.mkdir("assignment")
+
+    assignmentPath = 'assignment/' + time +".txt"
+    f = open(assignmentPath, 'w')
+    f.write(deadline)
+    f.write(" ")
+    f.write(assignmentName)
+    f.close()
+    print("処理完了")
+    return render_template("uploadSuccess.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
